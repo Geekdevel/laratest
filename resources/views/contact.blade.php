9c981e00ee1c7fc73eaef18cebf0aa5967b7c4a9
MacCube <maccube@MacCubes-iMac.local>
2019-11-25 18:50:42 +0200

    New Laravel: registration

--- resources/views/contact.blade.php
+++
@@ -1,5 +0,0 @@
-@extends ('loyaut')
-
-@section ('content')
-    <h1>This contact</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,61 +11,12 @@
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-    //return ['foo' => 'bar'];
-});*/
-
-/*Route::get('test', function(){
-    $name = request('name');
-
-    //return view('test');
-    return view('test', [
-        'name' => $name
-    ]);
-});*/
-
-/*Route::get('/post/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello, this is my first post!',
-        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-    ];
-
-    if (! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-    ]);
-});*/
-
-Route::get('/posts/{post}', 'PostsController@show');
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::get('/contact', function () {
-    return view('contact');
-});*/
+Auth::routes();
 
-/*Route::get('/about', function () {
-    return view('about');
-});*/
-
-Route::get('/about', function () {
-    return view('about', [
-        'articles' => App\Article::take(3)->latest()->get()
-    ]);
-});
-
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::post('/articles', 'ArticlesController@store');
-Route::get('/articles/{articles}', 'ArticlesController@show')->name('articles.show');
-Route::get('/articles/{articles}/edit', 'ArticlesController@edit');
-Route::put('/articles/{articles}', 'ArticlesController@update');
-
-//REST
-//Route::resurse('project', 'ProjectsController');
+Route::get('/home', 'HomeController@index')
+    ->name('home')
+    ->middleware('auth');
