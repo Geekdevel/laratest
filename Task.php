875eb44a6c67d88ca77966be33790ea27ec70a3b
MacCube <maccube@MacCubes-iMac.local>
2019-11-27 14:56:42 +0200

    Task

---
+++ Task.php
@@ -0,0 +1,15 @@
+<?php
+
+class Task
+{
+    public $description;
+
+    public function __construct($description)
+    {
+        var_dump($description);
+    }
+}
+
+$task = new Task('Learn OOP');
+
+var_dump($task->description);
