712310076c910e46c00fcc0da87f8c4c5040d163
MacCube <maccube@MacCubes-iMac.local>
2019-11-27 15:40:53 +0200

    Getter and Setter lessons end

--- Person.php
+++ Person.php
@@ -10,15 +10,25 @@ class Person
         $this->name = $name;
     }
 
+    public function getAge()
+    {
+        return $this->age*365;
+    }
+
     public function setAge($age)
     {
+        if ($age<18){
+            throw new Exception("Person is not old enough.");
+        }
         $this->age = $age;
     }
 }
 
 $john = new Person('John Doe');
 //$john->age = 30;
-$john->setAge(30);
+//$john->setAge(30);
+//$john->age = 3;
+$john->setAge(3); //public
 
 
-var_dump($john);
+var_dump($john->getAge());
