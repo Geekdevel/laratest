09d680ec6b73bef50f0f85ef473b13fbf6994d8b
MacCube <maccube@MacCubes-iMac.local>
2019-11-27 15:12:27 +0200

    Classes lessons first end

--- Task.php
+++ Task.php
@@ -3,13 +3,21 @@
 class Task
 {
     public $description;
+    public $completed = false;
 
     public function __construct($description)
     {
-        var_dump($description);
+        //var_dump($description);
+        $this->description = $description;
+    }
+    public function complete()
+    {
+        $this->completed = true;
     }
 }
 
-$task = new Task('Learn OOP');
+$task = new Task('Learn OOP', 'The teacher bla bla bla ...');
 
-var_dump($task->description);
+$task->complete();
+
+var_dump($task->completed);
